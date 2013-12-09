e9e4654157aa6f2db3f27f94299a649083c89c7c
wrs <252797991@qq.com>
2013-12-09 15:00:08 +0800

    init

---
+++ mingxinhg/DetailPropViewController.h
@@ -0,0 +1,14 @@
+//
+//  DetailPropViewController.h
+//  mingxinhg
+//
+//  Created by athudong on 13-12-9.
+//  Copyright (c) 2013年 athudong. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface DetailPropViewController : BaseViewController
+- (IBAction)backAction:(id)sender;
+
+@end
---
+++ mingxinhg/DetailPropViewController.m
@@ -0,0 +1,41 @@
+//
+//  DetailPropViewController.m
+//  mingxinhg
+//
+//  Created by athudong on 13-12-9.
+//  Copyright (c) 2013年 athudong. All rights reserved.
+//
+
+#import "DetailPropViewController.h"
+
+@interface DetailPropViewController ()
+
+@end
+
+@implementation DetailPropViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)backAction:(id)sender {
+    [self.navigationController popViewControllerAnimated:YES];
+}
+@end
